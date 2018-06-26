771050d41b9aa719b41c76952a31fe668b6d45ec
bdurb <brian@rpmgreenville.com>
2018-06-25 17:59:32 -0700

    added reactstrap, working on the styling of the searchbar, imported images

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,12 +1,16 @@
 import React from "react";
+import './searchbar.css';
+import insta from '../../assets/insta.png';
+import instacam from '../../assets/instacam.png';
 
 const SearchBar = () => {
   return (
-    <div>
-      <h1>Hello There</h1>
-      <form>
-        <input />
-      </form>
+    <div className="search-bar">
+      <img src={instacam} alt="insta"/>
+      <img src={insta} alt="insta"/>
+      <input
+        placeholder="search"
+      />
     </div>
   );
 };
