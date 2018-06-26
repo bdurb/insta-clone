242eca7b96c8fdb432aa3f04f83f1cc467b3f6b7
bdurb <brian@rpmgreenville.com>
2018-06-26 10:43:19 -0700

    working on styling of the post container

--- src/components/PostContainer/PostContainer.js
+++ src/components/PostContainer/PostContainer.js
@@ -5,11 +5,11 @@ import './postContainer.css';
 const PostContainer = props => {
   return (
     <div className="post-container">
-    <img src={props.post.thumbnailUrl} alt="thumbnail" />
-    {props.post.username}
-    <img src={props.post.imageUrl} alt="main"/>
-    {props.post.timestamp}
-    {props.post.comments.map(comment => (
+      <img src={props.post.thumbnailUrl} alt="thumbnail" />
+        {props.post.username}
+      <img src={props.post.imageUrl} alt="main"/>
+        {props.post.timestamp}
+        {props.post.comments.map(comment => (
       <CommentSection key={Math.random()} comment={comment} />
     ))}
     </div>
